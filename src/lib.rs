1eec974a9cb5c3e066794f30537b2312c51a3c45
Jeff Muizelaar <jrmuizel@gmail.com>
2019-02-18 14:08:39 -0500

    Fill out the a bunch of necessary impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,69 @@
+use std::marker::PhantomData;
+use std::mem::size_of;
+
 pub trait BincodeMaxSize {
     fn bincode_max_size() -> usize;
 }
 
-impl BincodeMaxSize for i32 {
-    fn bincode_max_size() -> usize {
-        4
+macro_rules! primitive_impls {
+    ($($ty:ident),*) => {
+        $(
+        impl BincodeMaxSize for $ty {
+            fn bincode_max_size() -> usize {
+                size_of::<Self>()
+            }
+        }
+        )*
     }
 }
 
-impl BincodeMaxSize for u32 {
-    fn bincode_max_size() -> usize {
-        4
+primitive_impls!(u8, i8, u16, i16, u32, i32, f32, f64, u64, i64);
+
+macro_rules! array_impls {
+    ($($len:tt),*) => {
+        $(
+        impl<T> BincodeMaxSize for [T; $len]
+        where
+            T: BincodeMaxSize
+        {
+            fn bincode_max_size() -> usize {
+                <T as BincodeMaxSize>::bincode_max_size() * $len
+            }
+        }
+        )*
     }
 }
 
-impl BincodeMaxSize for bool {
-    fn bincode_max_size() -> usize {
-        1
+array_impls!( 0,  1,  2,  3,  4,  5,  6,  7,
+             08, 09, 10, 11, 12, 13, 14, 15,
+             16, 17, 18, 19, 20, 21, 22, 23,
+             24, 25, 26, 27, 28, 29, 30, 31,
+             32);
+
+macro_rules! tuple_impl {
+    ($($tyvar:ident),*) => {
+        impl<$($tyvar: BincodeMaxSize),*> BincodeMaxSize for ( $( $tyvar ),* ,)
+        {
+            fn bincode_max_size() -> usize {
+                0 $( + <$tyvar as BincodeMaxSize>::bincode_max_size() )*
+            }
+        }
     }
 }
 
-impl BincodeMaxSize for f32 {
-    fn bincode_max_size() -> usize {
-        4
-    }
-}
+tuple_impl!(A);
+tuple_impl!(A, B);
+tuple_impl!(A, B, C);
+tuple_impl!(A, B, C, D);
+tuple_impl!(A, B, C, D, E);
+tuple_impl!(A, B, C, D, E, F);
+tuple_impl!(A, B, C, D, E, F, G);
+tuple_impl!(A, B, C, D, E, F, G, H);
+tuple_impl!(A, B, C, D, E, F, G, H, I);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J, K);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L);
 
-impl BincodeMaxSize for f64 {
-    fn bincode_max_size() -> usize {
-        8
-    }
-}
-
-impl BincodeMaxSize for i64 {
-    fn bincode_max_size() -> usize {
-        8
-    }
-}
-
-impl BincodeMaxSize for u64 {
-    fn bincode_max_size() -> usize {
-        8
-    }
-}
 
 impl<T: BincodeMaxSize> BincodeMaxSize for Option<T> {
     fn bincode_max_size() -> usize {
@@ -50,11 +71,8 @@ impl<T: BincodeMaxSize> BincodeMaxSize for Option<T> {
     }
 }
 
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+impl<T: BincodeMaxSize> BincodeMaxSize for PhantomData<T> {
+    fn bincode_max_size() -> usize {
+        0
     }
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,13 +1,21 @@
 use bincode_maxsize_derive::BincodeMaxSize;
 use bincode_maxsize::BincodeMaxSize;
+
+#[derive(BincodeMaxSize)]
+pub struct Bar {
+    a: [u32; 5],
+    b: (u8, u8)
+}
+
 #[derive(BincodeMaxSize)]
 pub enum Foo {
     M(i32),
     O(u32, u32, u32),
-    P(Option<f64>, f64)
+    P(Option<f64>, f64),
+    Q(Bar)
 }
 
 #[test]
-fn works() {
-    println!("{}", Foo::bincode_max_size());
+fn it_works() {
+    assert_eq!(Foo::bincode_max_size(), 26);
 }
